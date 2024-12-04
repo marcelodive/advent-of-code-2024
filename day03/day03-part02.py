197ffd02fb8dd71b455a2bcad881c4446a93ef87
Marcelo Pereira Rodrigues <marcelo@hotmail.com>
2024-12-04 18:36:52 -0300

    day 03 WIP

--- day03/day03-part02.py
+++ day03/day03-part02.py
@@ -3,12 +3,13 @@ import re
 
 input = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
 
-
+# Create arrays without don't()s, where every element will start with invalid instructions until find a do()
 splitByDonts = input.split("don't()")
 
 validInstructions = splitByDonts[0]
 for chunk in splitByDonts:
-    validInstructions += chunk.split("do()")[1] if len(chunk.split("do()")) > 1 else '' 
+    # Here I remove the invalid instructions (before the do()s) and keep only the valid instructions after the do()
+    validInstructions += ''.join(chunk.split("do()")[1:]) if len(chunk.split("do()")) > 1 else '' 
 
 pattern = r'mul\((\d+),(\d+)\)'
 matches = re.findall(pattern, validInstructions)
