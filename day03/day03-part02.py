d95a53aee3b0968e97bf02885a255f6c9e1d2190
Marcelo Pereira Rodrigues <marcelo@hotmail.com>
2024-12-04 19:10:43 -0300

    day 03 WIP

--- day03/day03-part02.py
+++ day03/day03-part02.py
@@ -7,7 +7,7 @@ input = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8
 # Create arrays without don't()s, where every element will start with invalid instructions until we find a do()
 splitByDonts = input.split("don't()")
 
-# The instructions start valid
+# The instructions start valid. I'm adding a pipe to avoid this case: mudon't()#!/usr/bin/python3mul(3,43)do()l(7,9)mul(6,9) (it should be 54)
 validInstructions = splitByDonts[0] + '|'
 for chunk in splitByDonts:
     # Here I remove the invalid instructions (before the do()s) and keep only the valid instructions after the do()
