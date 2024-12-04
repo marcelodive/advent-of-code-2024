553f41fb96624f45e8989ef0531857c9b23aea38
Marcelo Pereira Rodrigues <marcelo@hotmail.com>
2024-12-04 19:07:51 -0300

    day 03 WIP

--- day03/day03-part02.py
+++ day03/day03-part02.py
@@ -11,7 +11,7 @@ splitByDonts = input.split("don't()")
 validInstructions = splitByDonts[0]
 for chunk in splitByDonts:
     # Here I remove the invalid instructions (before the do()s) and keep only the valid instructions after the do()
-    validInstructions += ''.join(chunk.split("do()")[1:]) if len(chunk.split("do()")) > 1 else '' 
+    validInstructions += '|'.join(chunk.split("do()")[1:]) if len(chunk.split("do()")) > 1 else '|'
     
 print(validInstructions)
 
